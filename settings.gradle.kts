abdf5556c3aebfb00498b4087004d69e228b0e9b
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2024-01-12 02:57:00 +0900

    Chore: view binding 및 dependencies 설정 #39 (#40)
    * Chore: view binding 설정 #39
    
    * Chore: dependencies 설정 #39

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,12 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+        maven {
+            url = uri("https://devrepo.kakao.com/nexus/content/groups/public/")
+        }
     }
 }
 
