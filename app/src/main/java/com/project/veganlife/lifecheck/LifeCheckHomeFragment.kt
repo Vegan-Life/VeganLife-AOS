df04acab7e2a925bba967842b74302fe92f9684e
june0103 <rhwns0103@naver.com>
2024-01-21 23:14:15 +0900

    Design: 라이프체크 홈 UI 구현 #12

---
+++ app/src/main/java/com/project/veganlife/lifecheck/LifeCheckHomeFragment.kt
@@ -0,0 +1,27 @@
+package com.project.veganlife.lifecheck
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.project.veganlife.databinding.FragmentLifecheckHomeBinding
+
+class LifeCheckHomeFragment : Fragment() {
+
+    private var _binding: FragmentLifecheckHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLifecheckHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
