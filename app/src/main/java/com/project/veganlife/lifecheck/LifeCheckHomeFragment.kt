29af8c5574c3d62e04643cf92bb77cc2268fcefe
june0103 <rhwns0103@naver.com>
2024-01-22 01:40:35 +0900

    Design: 라이프체크 홈 viewpager 연결 #12

--- app/src/main/java/com/project/veganlife/lifecheck/LifeCheckHomeFragment.kt
+++ app/src/main/java/com/project/veganlife/lifecheck/LifeCheckHomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
 import com.project.veganlife.databinding.FragmentLifecheckHomeBinding
 
 class LifeCheckHomeFragment : Fragment() {
@@ -17,6 +18,30 @@ class LifeCheckHomeFragment : Fragment() {
     ): View {
         _binding = FragmentLifecheckHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupViewPager()
+    }
+
+    private fun setupViewPager() {
+
+        val adapter = LifeCheckHomeViewPagerAdapter(this)
+        binding.vpLifecheckHome.adapter = adapter
+
+        TabLayoutMediator(
+            binding.tablayoutLifecheckHome,
+            binding.vpLifecheckHome
+        ) { tab, position ->
+            tab.text = when (position) {
+                0 -> "일"
+                1 -> "주"
+                2 -> "월"
+                else -> "년"
+            }
+        }.attach()
     }
 
     override fun onDestroyView() {
