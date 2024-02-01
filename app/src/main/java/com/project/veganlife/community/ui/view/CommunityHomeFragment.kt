926909aad7be61cbe4d58137138671f656d860dd
hxeyexn <kck85075@kumoh.ac.kr>
2024-02-02 05:51:32 +0900

    Feat: 커뮤니티 홈 화면 전체 피드 RecyclerView에 통신한 데이터 연결 #55

--- app/src/main/java/com/project/veganlife/community/ui/view/CommunityHomeFragment.kt
+++ app/src/main/java/com/project/veganlife/community/ui/view/CommunityHomeFragment.kt
@@ -5,23 +5,53 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.project.veganlife.community.ui.adapter.FeedsAdapter
+import com.project.veganlife.community.ui.viewmodel.FeedsGetViewModel
 import com.project.veganlife.databinding.FragmentCommunityHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CommunityHomeFragment : Fragment() {
-
     private var _binding: FragmentCommunityHomeBinding? = null
     private val binding get() = _binding!!
 
+    private val feedsGetViewModel: FeedsGetViewModel by viewModels()
+    private lateinit var feedsAdapter: FeedsAdapter
+    private var accessToken = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrY2s4NTA3NUBuYXZlci5jb20iLCJleHAiOjE3MDY4MjAyNjUsImlhdCI6MTcwNjgxMzA2NX0.JGWL_LBvau2l-ypmedIhCXWsYVI6UOJ1z7Ig4BC4qZw"
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentCommunityHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        feedsAdapter = FeedsAdapter()
+        feedsGetViewModel.getFeeds(accessToken)
+        feedsGetViewModel.feedsGet.observe(viewLifecycleOwner) { response ->
+            feedsAdapter.submitList(response.content)
+        }
+        initFeedsRecyclerView()
+    }
+
+    private fun initFeedsRecyclerView() {
+        binding.rvCommunityhomeFeed.apply {
+            adapter = feedsAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
