21e994754093fdf8347e37b716252e3a5f83d001
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-03-17 16:26:06 +0900

    Feat: 로그인을 위한 ViewModel 구현 #64

---
+++ app/src/main/java/com/project/veganlife/login/ui/viewmodel/LoginViewModel.kt
@@ -0,0 +1,26 @@
+package com.project.veganlife.login.ui.view.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.project.veganlife.login.data.model.LoginResponse
+import com.project.veganlife.login.domain.usecase.LoginUsecase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val loginUseCase: LoginUsecase,
+) : ViewModel() {
+    private val _loginResponse = MutableLiveData<LoginResponse?>()
+    val loginResponse get() = _loginResponse
+
+    suspend fun login(provider: String,context: Context) {
+        viewModelScope.launch {
+            val getAccessToken = loginUseCase(provider,context)
+            _loginResponse.value = getAccessToken
+        }
+    }
+}
