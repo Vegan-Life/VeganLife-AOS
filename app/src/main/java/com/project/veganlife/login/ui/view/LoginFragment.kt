d2460688ec0c1eb847dedf86dbf181ad923fa535
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-03-17 16:27:16 +0900

    Feat: 로그인 기능 구현 #64

---
+++ app/src/main/java/com/project/veganlife/login/ui/view/LoginFragment.kt
@@ -0,0 +1,72 @@
+package com.project.veganlife.login.ui.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.project.veganlife.R
+import com.project.veganlife.databinding.FragmentLoginBinding
+import com.project.veganlife.login.ui.view.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    private val loginViewModel: LoginViewModel by viewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLoginKakao.setOnClickListener {
+            login("kakao")
+        }
+
+        binding.btnLoginNaver.setOnClickListener {
+            login("naver")
+        }
+
+        loginAfterMoveFragment()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun loginAfterMoveFragment() {
+        loginViewModel.loginResponse.observe(viewLifecycleOwner) { loginResponse ->
+            if (loginResponse != null) {
+                if (loginResponse.hasAdditionalInfo == true) {
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                } else {
+                    findNavController().navigate(R.id.action_loginFragment_to_signupVeganTypeFragment)
+                }
+            }
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun login(provider: String) {
+        GlobalScope.launch {
+            context?.let { loginViewModel.login(provider, it) }
+        }
+    }
+}
