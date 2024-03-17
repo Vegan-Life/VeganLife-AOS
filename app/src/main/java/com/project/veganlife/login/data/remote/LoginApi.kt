9d8f154913e209a3761dadf93bf2b4e7d1e8df9d
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-03-17 16:16:32 +0900

    Feat: 로그인을 위한 api 구현   #64

---
+++ app/src/main/java/com/project/veganlife/login/data/remote/LoginApi.kt
@@ -0,0 +1,17 @@
+package com.project.veganlife.login.data.model.remote
+
+import com.project.veganlife.login.data.model.LoginProvider
+import com.project.veganlife.login.data.model.LoginRequest
+import com.project.veganlife.login.data.model.LoginResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface LoginApi {
+    @POST("members/oauth/{provider}/login")
+    suspend fun getAccessToken(
+        @Path("provider") provider: String,
+        @Body accessToken: LoginRequest,
+    ): Response<LoginResponse>
+}
