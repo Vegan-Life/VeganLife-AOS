92c4a7f431c682a6239d465fe1777fe1eb70c845
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-03-18 05:09:48 +0900

    Refactor: LoginUsecase에서 내부 데이터 저장 구현 삭제 #64

--- app/src/main/java/com/project/veganlife/login/domain/usecase/LoginUsecase.kt
+++ app/src/main/java/com/project/veganlife/login/domain/usecase/LoginUsecase.kt
@@ -1,8 +1,6 @@
 package com.project.veganlife.login.domain.usecase
 
 import android.content.Context
-import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKeys
 import com.project.veganlife.login.data.model.LoginRequest
 import com.project.veganlife.login.data.model.LoginResponse
 import com.project.veganlife.login.domain.repository.LoginRepository
@@ -12,25 +10,6 @@ class LoginUsecase @Inject constructor(val loginRepository: LoginRepository) {
     suspend operator fun invoke(loginProvider: String, context: Context): LoginResponse? {
         val sdkAccessTokenResult = loginRepository.login(loginProvider)
         val apiResponseResult = loginRepository.loginApi(loginProvider, LoginRequest(sdkAccessTokenResult))
-
-        // data store에 저장
-        val prefFileName = "encrypted_prefs"
-        val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
-
-        val sharedPreferences = EncryptedSharedPreferences.create(
-            prefFileName,
-            masterKeyAlias,
-            context,
-            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-        )
-
-        sharedPreferences.edit().apply{
-            if (apiResponseResult != null) {
-                putString("ApiAccessToken",apiResponseResult.accessToken)
-                putString("ApiRefreshToken",apiResponseResult.refreshToken)
-            }
-        }.apply()
 
         return apiResponseResult
     }
