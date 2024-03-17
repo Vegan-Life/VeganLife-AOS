c6db8a51a3639aa04c588a7d871fb3b7ddce95a1
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-03-17 16:23:04 +0900

    Feat: sns과 api 로그인 구현을 위한 repository 구현체 추가 #64

---
+++ app/src/main/java/com/project/veganlife/login/data/repositoryImpl/LoginRepositoryImpl.kt
@@ -0,0 +1,34 @@
+package com.project.veganlife.login.data.repositoryImpl
+
+import com.project.veganlife.login.data.datasource.KakaoLoginDataSource
+import com.project.veganlife.login.data.datasource.NaverLoginDataSource
+import com.project.veganlife.login.data.model.LoginRequest
+import com.project.veganlife.login.data.model.LoginResponse
+import com.project.veganlife.login.data.remote.LoginRemoteDataSource
+import com.project.veganlife.login.domain.repository.LoginRepository
+import javax.inject.Inject
+
+class LoginRepositoryImpl @Inject constructor(
+    private val kakaoLoginDataSource: KakaoLoginDataSource,
+    private val naverLoginDataSource: NaverLoginDataSource,
+    private val loginRemoteDataSource: LoginRemoteDataSource,
+) : LoginRepository {
+
+    override suspend fun login(provider: String): String {
+        var accessToken = ""
+        if (provider == "kakao") {
+            accessToken = kakaoLoginDataSource.login()
+
+        } else if (provider == "naver") {
+            accessToken = naverLoginDataSource.login()
+        }
+        return accessToken
+    }
+
+    override suspend fun loginApi(
+        provider: String,
+        accessToken: LoginRequest?,
+    ): LoginResponse? {
+        return accessToken?.let { loginRemoteDataSource.loginApi(provider, it) }
+    }
+}
