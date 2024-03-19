87628b81a1c61780a6c707b33006be446f64c43f
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-03-19 21:31:46 +0900

    Refactor: LoginUsecase 수정 #64 토큰 저장 추가

--- app/src/main/java/com/project/veganlife/login/domain/usecase/LoginUsecase.kt
+++ app/src/main/java/com/project/veganlife/login/domain/usecase/LoginUsecase.kt
@@ -6,10 +6,14 @@ import com.project.veganlife.login.data.model.LoginResponse
 import com.project.veganlife.login.domain.repository.LoginRepository
 import javax.inject.Inject
 
-class LoginUsecase @Inject constructor(val loginRepository: LoginRepository) {
+class LoginUsecase @Inject constructor(val loginRepository: LoginRepository,val userUsecase: UserUsecase) {
     suspend operator fun invoke(loginProvider: String, context: Context): LoginResponse? {
         val sdkAccessTokenResult = loginRepository.login(loginProvider)
         val apiResponseResult = loginRepository.loginApi(loginProvider, LoginRequest(sdkAccessTokenResult))
+
+        if (apiResponseResult != null) {
+            userUsecase(apiResponseResult)
+        }
 
         return apiResponseResult
     }
