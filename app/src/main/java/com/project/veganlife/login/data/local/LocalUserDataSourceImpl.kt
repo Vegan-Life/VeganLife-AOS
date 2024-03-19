335162bd30995ed8b0b7898c854330db6ba279b6
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-03-19 21:31:19 +0900

    Refactor: LocalUserDataSourceImpl 수정 #64

--- app/src/main/java/com/project/veganlife/login/data/local/LocalUserDataSourceImpl.kt
+++ app/src/main/java/com/project/veganlife/login/data/local/LocalUserDataSourceImpl.kt
@@ -5,20 +5,18 @@ import com.project.veganlife.login.data.model.LoginResponse
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class LocalUserDataSourceImpl @Inject constructor(
-    private var sharedPreferences: SharedPreferences,
+    private val sharedPreferences: SharedPreferences,
 ) : LocalUserDataSource {
     override suspend fun saveToken(token: LoginResponse) {
-
-        val saveToken = CoroutineScope(Dispatchers.IO).async {
+        withContext(Dispatchers.IO) {
             sharedPreferences.edit().apply {
                 putString("ApiAccessToken", token.accessToken)
                 putString("ApiRefreshToken", token.refreshToken)
             }.commit()
         }
-
-        saveToken.await()
     }
 }
