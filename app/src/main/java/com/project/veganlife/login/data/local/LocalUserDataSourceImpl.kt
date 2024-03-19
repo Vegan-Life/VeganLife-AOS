b1ee4ac9548f748f4cc661529960797cd2ec62d4
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-03-19 16:28:28 +0900

    Refactor: LocalUserDataSourceImpl 수정 #64 SharedPreferences 주입 받아서 사용으로 변경

--- app/src/main/java/com/project/veganlife/login/data/local/LocalUserDataSourceImpl.kt
+++ app/src/main/java/com/project/veganlife/login/data/local/LocalUserDataSourceImpl.kt
@@ -1,31 +1,24 @@
 package com.project.veganlife.login.data.local
 
-import android.content.Context
-import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKeys
+import android.content.SharedPreferences
 import com.project.veganlife.login.data.model.LoginResponse
-import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import javax.inject.Inject
 
-class LocalUserDataSourceImpl(
-    @ApplicationContext private val context: Context,
+class LocalUserDataSourceImpl @Inject constructor(
+    private var sharedPreferences: SharedPreferences,
 ) : LocalUserDataSource {
     override suspend fun saveToken(token: LoginResponse) {
 
-        val prefFileName = "encrypted_prefs"
-        val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        val saveToken = CoroutineScope(Dispatchers.IO).async {
+            sharedPreferences.edit().apply {
+                putString("ApiAccessToken", token.accessToken)
+                putString("ApiRefreshToken", token.refreshToken)
+            }.commit()
+        }
 
-        val sharedPreferences = EncryptedSharedPreferences.create(
-            prefFileName,
-            masterKeyAlias,
-            context,
-            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-        )
-
-        sharedPreferences.edit().apply {
-            putString("ApiAccessToken", token.accessToken)
-            putString("ApiRefreshToken", token.refreshToken)
-        }.apply()
-
+        saveToken.await()
     }
 }
