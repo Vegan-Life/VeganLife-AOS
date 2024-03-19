54afe2e3a48ae408e3f4b4e16c75358dc5886abf
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-03-19 22:19:14 +0900

    Refactor: LoginViewModel 수정 #64 sharedPreferences 주입

--- app/src/main/java/com/project/veganlife/login/ui/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/project/veganlife/login/ui/viewmodel/LoginViewModel.kt
@@ -1,12 +1,12 @@
 package com.project.veganlife.login.ui.view.viewmodel
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.project.veganlife.login.data.model.LoginResponse
 import com.project.veganlife.login.domain.usecase.LoginUsecase
-import com.project.veganlife.login.domain.usecase.UserUsecase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -14,11 +14,12 @@ import javax.inject.Inject
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val loginUseCase: LoginUsecase,
+    private val sharedPreferences: SharedPreferences
 ) : ViewModel() {
     private val _loginResponse = MutableLiveData<LoginResponse?>()
     val loginResponse get() = _loginResponse
 
-    suspend fun login(provider: String,context: Context) {
+    fun login(provider: String,context: Context) {
         viewModelScope.launch {
             val getAccessToken = loginUseCase(provider,context)
             _loginResponse.value = getAccessToken
