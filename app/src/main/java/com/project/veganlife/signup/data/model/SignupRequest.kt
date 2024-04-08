6b205b7aefcf194d57c31b33352179d6fe7e1296
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-04-08 17:20:22 +0900

    Rename: data class 오타 교정 #60

--- app/src/main/java/com/project/veganlife/signup/data/model/SignupRequest.kt
+++ app/src/main/java/com/project/veganlife/signup/data/model/SignupRequest.kt
@@ -2,7 +2,7 @@ package com.project.veganlife.signup.data.model
 
 data class SignupRequest(
     val nickname: String,
-    val gendar: String,
+    val gender: String,
     val vegetarianType: String,
     val birthYear: Int,
     val height: Int,
