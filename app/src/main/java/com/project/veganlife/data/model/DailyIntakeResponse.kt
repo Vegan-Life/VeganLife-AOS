bd58601e8386599034432460d69791eb57f42ef3
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-04-13 04:43:36 +0900

    Feat: Home 권장 섭취량 및 일일 섭취량 data class 추가 #75

---
+++ app/src/main/java/com/project/veganlife/data/model/DailyIntakeResponse.kt
@@ -0,0 +1,8 @@
+package com.project.veganlife.data.model
+
+data class DailyIntakeResponse(
+    val calorie: Int,
+    val carbs: Int,
+    val protein: Int,
+    val fat: Int,
+)
---
+++ app/src/main/java/com/project/veganlife/data/model/RecommendedIntakeResponse.kt
@@ -0,0 +1,8 @@
+package com.project.veganlife.data.model
+
+data class RecommendedIntakeResponse(
+    val calorie: Int,
+    val carbs: Int,
+    val protein: Int,
+    val fat: Int,
+)
