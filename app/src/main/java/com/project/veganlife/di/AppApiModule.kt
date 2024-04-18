45349e49d4e28d938fcf6e57e1514f480dbc2a80
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-04-18 16:24:57 +0900

    Feat: Home 권장 섭취량 api module 추가 #75

--- app/src/main/java/com/project/veganlife/di/AppApiModule.kt
+++ app/src/main/java/com/project/veganlife/di/AppApiModule.kt
@@ -1,6 +1,7 @@
 package com.project.veganlife.di
 
 import com.project.veganlife.data.remote.ProfileInfoGetApi
+import com.project.veganlife.data.remote.RecommendedIntakeGetApi
 import com.project.veganlife.signup.data.remote.SignupApi
 import dagger.Module
 import dagger.Provides
@@ -19,5 +20,10 @@ class AppApiModule {
     @Provides
     fun provideHomeProfilePhotoApi(retrofit: Retrofit): ProfileInfoGetApi{
         return retrofit.create(ProfileInfoGetApi::class.java)
+    }
+
+    @Provides
+    fun provideRecommendedIntakeGetApi(retrofit: Retrofit): RecommendedIntakeGetApi{
+        return retrofit.create(RecommendedIntakeGetApi::class.java)
     }
 }
