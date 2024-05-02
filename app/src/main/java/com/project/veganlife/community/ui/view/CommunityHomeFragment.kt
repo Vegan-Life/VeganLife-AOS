417046923bf283fa97328eda3e0787aec1ef2fb2
gayeon00 <chlrkdus4819@gmail.com>
2024-05-02 16:53:22 +0900

    feat: 커뮤니티 홈 글쓰기 작성 버튼
    - 글쓰기 버튼 누르면 애니메이션 구현 완료

--- app/src/main/java/com/project/veganlife/community/ui/view/CommunityHomeFragment.kt
+++ app/src/main/java/com/project/veganlife/community/ui/view/CommunityHomeFragment.kt
@@ -1,14 +1,17 @@
 package com.project.veganlife.community.ui.view
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.project.veganlife.databinding.FragmentCommunityHomeBinding
+import com.project.veganlife.utils.ui.DisplayUtils
 
 class CommunityHomeFragment : Fragment() {
 
+    private var fabOpen = false
     private var _binding: FragmentCommunityHomeBinding? = null
     private val binding get() = _binding!!
 
@@ -17,6 +20,25 @@ class CommunityHomeFragment : Fragment() {
     ): View {
         _binding = FragmentCommunityHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.efabCommunityhomeWrite.setOnClickListener {
+
+            //피드 - 레시피  -> 4dp
+            //레시피 - 글쓰기 -> 12dp
+            if(!fabOpen) {
+                ObjectAnimator.ofFloat(binding.efabCommunityhomeWriteRecipe, "translationY", -1*DisplayUtils.dpToPx(requireContext(), 52f)).apply { start() }
+                ObjectAnimator.ofFloat(binding.efabCommunityhomeWriteFeed, "translationY", -1*DisplayUtils.dpToPx(requireContext(), 96f)).apply { start() }
+            } else {
+                ObjectAnimator.ofFloat(binding.efabCommunityhomeWriteRecipe, "translationY", 0f).apply { start() }
+                ObjectAnimator.ofFloat(binding.efabCommunityhomeWriteFeed, "translationY", 0f).apply { start() }
+            }
+
+            fabOpen = !fabOpen
+
+        }
     }
 
     override fun onDestroyView() {
