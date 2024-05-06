ebc87cc03a8b0e6d9953345d1a5775bc9274c692
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-05-07 06:30:25 +0900

    Feat: 홈 일일 섭취량 repository 구현체 구현 #75

---
+++ app/src/main/java/com/project/veganlife/home/data/repositoryImpl/HomeDailyIntakeGetRepositoryImpl.kt
@@ -0,0 +1,15 @@
+package com.project.veganlife.home.data.repositoryImpl
+
+import com.project.veganlife.data.model.ApiResult
+import com.project.veganlife.data.model.DailyIntakeResponse
+import com.project.veganlife.home.data.datasource.HomeDailyIntakeGetDataSource
+import com.project.veganlife.home.domain.repository.HomeDailyIntakeGetRepository
+import javax.inject.Inject
+
+class HomeDailyIntakeGetRepositoryImpl @Inject constructor(
+    private val homeDailyIntakeGetDataSource: HomeDailyIntakeGetDataSource
+): HomeDailyIntakeGetRepository{
+    override suspend fun getRecommendedIntake(): ApiResult<DailyIntakeResponse>? {
+        return homeDailyIntakeGetDataSource.getDailyIntake()
+    }
+}
