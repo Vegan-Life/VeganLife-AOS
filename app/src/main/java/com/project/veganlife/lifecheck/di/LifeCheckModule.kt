e4673771bdeb91fb7f947e8857ab6de3acf256c4
june0103 <rhwns0103@naver.com>
2024-05-23 23:09:37 +0900

    Feat: 라이프체크 권장섭취량조회Hilt 종속성 주입 #77

--- app/src/main/java/com/project/veganlife/lifecheck/di/LifeCheckModule.kt
+++ app/src/main/java/com/project/veganlife/lifecheck/di/LifeCheckModule.kt
@@ -2,18 +2,24 @@ package com.project.veganlife.lifecheck.di
 
 import com.project.veganlife.lifecheck.data.datasource.LifeCheckDailyIntakeDataSource
 import com.project.veganlife.lifecheck.data.datasource.LifeCheckDailyIntakeDataSourceImpl
+import com.project.veganlife.lifecheck.data.datasource.LifeCheckRecommendedIntakeDataSource
+import com.project.veganlife.lifecheck.data.datasource.LifeCheckRecommendedIntakeDataSourceImpl
 import com.project.veganlife.lifecheck.data.ropositoryImpl.LifeCheckDailyIntakeRepositoryImpl
+import com.project.veganlife.lifecheck.data.ropositoryImpl.LifeCheckRecommendedIntakeRepositoryImpl
 import com.project.veganlife.lifecheck.domain.repository.LifeCheckDailyIntakeRepository
+import com.project.veganlife.lifecheck.domain.repository.LifeCheckRecommendedIntakeRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object LifeCheckDailyIntakeModule {
+object LifeCheckModule {
 
     @Provides
+    @Singleton
     fun provideLifeCheckDailyIntakeRepository(
         lifeCheckDailyIntakeRepositoryImpl: LifeCheckDailyIntakeRepositoryImpl
     ): LifeCheckDailyIntakeRepository {
@@ -21,9 +27,26 @@ object LifeCheckDailyIntakeModule {
     }
 
     @Provides
+    @Singleton
     fun provideLifeCheckDailyIntakeDataSource(
         lifeCheckDailyIntakeDataSourceImpl: LifeCheckDailyIntakeDataSourceImpl
     ): LifeCheckDailyIntakeDataSource {
         return lifeCheckDailyIntakeDataSourceImpl
+    }
+
+    @Provides
+    @Singleton
+    fun provideLifeCheckRecommendedIntakeRepository(
+        lifeCheckRecommendedIntakeRepositoryImpl: LifeCheckRecommendedIntakeRepositoryImpl
+    ): LifeCheckRecommendedIntakeRepository {
+        return lifeCheckRecommendedIntakeRepositoryImpl
+    }
+
+    @Provides
+    @Singleton
+    fun provideLifeCheckRecommendedIntakeDataSource(
+        lifeCheckRecommendedIntakeDataSourceImpl: LifeCheckRecommendedIntakeDataSourceImpl
+    ): LifeCheckRecommendedIntakeDataSource {
+        return lifeCheckRecommendedIntakeDataSourceImpl
     }
 }
