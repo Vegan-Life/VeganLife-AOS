1b3878fd22853b16dd5674dc8dd232020253cb4f
june0103 <rhwns0103@naver.com>
2024-05-30 23:05:40 +0900

    Feat: 라이프체크 주간섭취칼로리 조회 API #88

---
+++ app/src/main/java/com/project/veganlife/lifecheck/data/remote/LifeCheckWeeklyCalorieGetApi.kt
@@ -0,0 +1,16 @@
+package com.project.veganlife.lifecheck.data.remote
+
+import com.project.veganlife.lifecheck.data.model.LifeCheckWeeklyCalorieResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface LifeCheckWeeklyCalorieGetApi {
+    @GET("members/nutrients/week")
+    suspend fun getWeeklyCalorie(
+        @Header("Authorization") accessToken: String?,
+        @Query("startDate") startDate: String,
+        @Query("endDate") endDate: String
+    ): Response<LifeCheckWeeklyCalorieResponse>?
+}
