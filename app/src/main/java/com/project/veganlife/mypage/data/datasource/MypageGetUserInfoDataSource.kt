82e06937a8bd6ccfe90c36dcbe5a2eb2ba59552d
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-06-12 05:27:00 +0900

    Remove: 마이페이지 유저 정보 조회 관련 중복코드 삭제 #74 메인 패키지에 존재함으로써 mypage 패키지에 있는 유저 정보 조회 관련 코드 삭제

--- app/src/main/java/com/project/veganlife/mypage/data/datasource/MypageGetUserInfoDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.project.veganlife.mypage.data.datasource
-
-import com.project.veganlife.data.model.ApiResult
-import com.project.veganlife.data.model.ProfileResponse
-
-interface MypageGetUserInfoDataSource {
-    suspend fun getUserInfo(): ApiResult<ProfileResponse>
-}
