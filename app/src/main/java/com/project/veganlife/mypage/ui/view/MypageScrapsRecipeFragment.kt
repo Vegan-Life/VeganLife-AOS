927b7662bc746aa8f52171f42c7b0dc73737909c
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-06-13 23:33:57 +0900

    Feat: 레시피 좋아요, 좋아요 취소 fragment 구현 #74

--- app/src/main/java/com/project/veganlife/mypage/ui/view/MypageScrapsRecipeFragment.kt
+++ app/src/main/java/com/project/veganlife/mypage/ui/view/MypageScrapsRecipeFragment.kt
@@ -5,15 +5,85 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.project.veganlife.R
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.project.veganlife.databinding.FragmentMypageScrapsRecipeBinding
+import com.project.veganlife.mypage.ui.adapter.MypageScrapedRecipeAdapter
+import com.project.veganlife.mypage.ui.viewmodel.MypageScrapedRecipeViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MypageScrapsRecipeFragment : Fragment() {
+    private var _binding: FragmentMypageScrapsRecipeBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var adapter: MypageScrapedRecipeAdapter
+
+    private val viewModel: MypageScrapedRecipeViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mypage_scraps_recipe, container, false)
+        _binding = FragmentMypageScrapsRecipeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setToolbarListener()
+
+        getScrapedRecipe()
+
+        // ui
+        setScrapedRecipe()
+
+        setRecipeLikeBackground()
+    }
+
+    private fun setToolbarListener() {
+        binding.toolbarMypageToolbar.run {
+            setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
+    }
+
+    private fun getScrapedRecipe() {
+        viewModel.getScrapedRecipe()
+    }
+
+    private fun setScrapedRecipe() {
+        lifecycleScope.launch {
+            adapter = MypageScrapedRecipeAdapter(viewModel)
+            binding.apply {
+                rvMypageRecipe.layoutManager = LinearLayoutManager(requireContext())
+                rvMypageRecipe.adapter = adapter
+            }
+
+            viewModel.scrapedRecipe.collectLatest { pagingData ->
+                pagingData?.let {
+                    adapter.submitData(it)
+                }
+            }
+        }
+    }
+
+    private fun setRecipeLikeBackground() {
+        viewModel.apply {
+            recipeLikeResponse.observe(viewLifecycleOwner) {
+                adapter.refresh()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
