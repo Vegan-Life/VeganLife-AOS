34bb87e5e5a63338e522519ff8e7259b2a3bb7a3
june0103 <rhwns0103@naver.com>
2024-06-21 00:27:07 +0900

    Feat: 라이프체크 식품 데이터 등록 API  #103

---
+++ app/src/main/java/com/project/veganlife/lifecheck/data/remote/LifeCheckMealDataPostApi.kt
@@ -0,0 +1,15 @@
+package com.project.veganlife.lifecheck.data.remote
+
+import com.project.veganlife.lifecheck.data.model.LifeCheckMealDataRequest
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface LifeCheckMealDataPostApi {
+    @POST("meal-data")
+    suspend fun registerMealData(
+        @Header("Authorization") token: String?,
+        @Body lifeCheckMealDataRequest: LifeCheckMealDataRequest
+    ): Response<LifeCheckMealDataRequest>?
+}
