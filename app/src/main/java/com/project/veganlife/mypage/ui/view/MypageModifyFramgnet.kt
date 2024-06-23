c795d90f84c3bae6769f22384f7db11b80914ce7
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-06-23 15:17:24 +0900

    임시

--- app/src/main/java/com/project/veganlife/mypage/ui/view/MypageModifyFramgnet.kt
+++ app/src/main/java/com/project/veganlife/mypage/ui/view/MypageModifyFramgnet.kt
@@ -1,19 +1,397 @@
 package com.project.veganlife.mypage.ui.view
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.project.veganlife.R
+import com.project.veganlife.databinding.FragmentMypageModifyFramgnetBinding
+import com.project.veganlife.mypage.data.model.ProfileModifyRequest
+import com.project.veganlife.mypage.ui.viewmodel.MypageViewmodel
+import com.project.veganlife.utils.ui.VeganTypeChange
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MypageModifyFramgnet : Fragment() {
+    private var _binding: FragmentMypageModifyFramgnetBinding? = null
+    private val binding get() = _binding!!
+
+    private val mypageViewmodel: MypageViewmodel by activityViewModels()
+
+    private val PICK_IMAGE_REQUEST = 1
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mypage_modify_framgnet, container, false)
+        _binding = FragmentMypageModifyFramgnetBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // toolbar
+        setToolbarListener()
+
+        replaceProfilePhoto()
+
+        // ui
+        setUserInfoUi()
+
+        selectVeganType()
+    }
+
+    private fun setToolbarListener() {
+        binding.toolbarMypageToolbar.run {
+            setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
+    }
+
+    private fun setUserInfoUi() {
+        mypageViewmodel.apply {
+            binding.apply {
+                profileInfoResponse.observe(viewLifecycleOwner) { profile ->
+                    Glide.with(requireContext()).load(profile.imageUrl).apply(
+                        RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
+                        .into(ivMypageProfile)
+
+                    tietMypageNickname.setText(profile.nickname)
+                    tietMypageNickname.isEnabled = true
+
+                    tietMypageId.setText(profile.email)
+                    tietMypageId.isEnabled = false
+
+                    tietMypageHeight.setText(profile.height.toString())
+                    tietMypageWeight.isEnabled = true
+
+                    tietMypageWeight.setText(profile.weight.toString())
+                    tietMypageWeight.isEnabled = true
+
+                    tietMypageAge.setText(profile.birthYear.toString())
+                    tietMypageAge.isEnabled = false
+
+                    setVeganTypeUi(profile.vegetarianType)
+                    setGenderUi(profile.gender)
+                }
+            }
+        }
+    }
+
+    private fun replaceProfilePhoto() {
+        binding.apply {
+            binding.ivMypagePhoto.setOnClickListener {
+                openGallery()
+            }
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            val imageUri: Uri? = data.data
+            if(imageUri != null) {
+                val filePath = getRealPathFromURI(imageUri)
+
+                Glide.with(this)
+                    .load(imageUri)
+                    .into(binding.ivMypageProfile)
+
+                Log.d("modify",imageUri.toString())
+                Log.d("modify filePath",filePath.toString())
+            }
+        }
+    }
+    private fun getRealPathFromURI(uri: Uri): String? {
+        var result: String? = null
+        val proj = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = activity?.contentResolver?.query(uri, proj, null, null, null)
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                val column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+                result = cursor.getString(column_index)
+            }
+            cursor.close()
+        }
+        return result
+    }
+
+    private fun setVeganTypeUi(Type: String) {
+        binding.apply {
+            when (Type) {
+                "VEGAN" -> {
+                    veganType.apply {
+                        ivMypageImage.setImageResource(R.drawable.all_vegan_type_vegan)
+                        tvMypageVeganType.setTextColor(
+                            ContextCompat.getColor(
+                                requireContext(),
+                                R.color.base3
+                            )
+                        )
+                        tvMypageVeganType.isSelected = true
+                    }
+                    lactoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_lacto_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                    ovoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_ovo_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                    lactoOvoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_lacto_ovo_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                    pescoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_pesco_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                }
+
+                "LACTO" -> {
+                    veganType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_vegan_gray)
+                        tvMypageVeganType.setTextColor(
+                            ContextCompat.getColor(
+                                requireContext(),
+                                R.color.gray2
+                            )
+                        )
+                        tvMypageVeganType.isSelected = false
+                    }
+                    lactoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.all_vegan_type_lacto)
+                        tvMypageVeganType.isSelected = true
+                    }
+                    ovoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_ovo_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                    lactoOvoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_lacto_ovo_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                    pescoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_pesco_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                }
+
+                "OVO" -> {
+                    veganType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_vegan_gray)
+                        tvMypageVeganType.setTextColor(
+                            ContextCompat.getColor(
+                                requireContext(),
+                                R.color.gray2
+                            )
+                        )
+                        tvMypageVeganType.isSelected = false
+                    }
+                    lactoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_lacto_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                    ovoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.all_vegan_type_ovo)
+                        tvMypageVeganType.isSelected = true
+                    }
+                    lactoOvoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_lacto_ovo_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                    pescoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_pesco_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                }
+
+                "LACTO_OVO" -> {
+                    veganType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_vegan_gray)
+                        tvMypageVeganType.setTextColor(
+                            ContextCompat.getColor(
+                                requireContext(),
+                                R.color.gray2
+                            )
+                        )
+                        tvMypageVeganType.isSelected = false
+                    }
+                    lactoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_lacto_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                    ovoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_ovo_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                    lactoOvoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.all_vegan_type_lacto_ovo)
+                        tvMypageVeganType.isSelected = true
+                    }
+                    pescoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_pesco_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                }
+
+                "PESCO" -> {
+                    veganType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_vegan_gray)
+                        tvMypageVeganType.setTextColor(
+                            ContextCompat.getColor(
+                                requireContext(),
+                                R.color.gray2
+                            )
+                        )
+                        tvMypageVeganType.isSelected = false
+                    }
+                    lactoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_lacto_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                    ovoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_ovo_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                    lactoOvoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.mypage_vegan_type_lacto_ovo_gray)
+                        tvMypageVeganType.isSelected = false
+                    }
+                    pescoType.apply {
+                        ivMypageImage.setImageResource(R.drawable.all_vegan_type_pesco)
+                        tvMypageVeganType.isSelected = true
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setGenderUi(gender: String) {
+        when (gender) {
+            "M" -> {
+                binding.apply {
+                    btnMypageMale.setBackgroundResource(R.color.base3)
+                    btnMypageMale.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.white
+                        )
+                    )
+                    btnMypageFemale.setBackgroundResource(R.color.gray1)
+                    btnMypageFemale.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.sub_gray2
+                        )
+                    )
+                }
+            }
+
+            "F" -> {
+                binding.apply {
+                    btnMypageFemale.setBackgroundResource(R.color.base3)
+                    btnMypageFemale.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.white
+                        )
+                    )
+                    btnMypageMale.setBackgroundResource(R.color.gray1)
+                    btnMypageMale.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.sub_gray2
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    private fun selectVeganType() {
+        mypageViewmodel.apply {
+
+            binding.apply {
+                veganType.clVeganTypeLayout.setOnClickListener {
+                    setVeganTypeUi("VEGAN")
+                    setVeganType("VEGAN")
+                }
+
+                lactoType.clLactoLayout.setOnClickListener {
+                    setVeganTypeUi("LACTO")
+                    setVeganType("LACTO")
+                }
+
+                ovoType.clOvoLayout.setOnClickListener {
+                    setVeganTypeUi("OVO")
+                    setVeganType("OVO")
+                }
+
+                lactoOvoType.clLactoOvoLayout.setOnClickListener {
+                    setVeganTypeUi("LACTO_OVO")
+                    setVeganType("LACTO_OVO")
+                }
+
+                pescoType.clPescoLayout.setOnClickListener {
+                    setVeganTypeUi("PESCO")
+                    setVeganType("PESCO")
+                }
+            }
+        }
+    }
+
+    private fun modifyUserInfo() {
+        mypageViewmodel.apply {
+            binding.apply {
+                btnMypageModify.setOnClickListener {
+                    if (
+                        isUserInfoStateCheck(
+                            ProfileModifyRequest(
+                                nickname = tietMypageNickname.text.toString(),
+                                imageUrl = "",
+                                vegetarianType = vegetarianType.value.toString(),
+                                gender = profileInfoResponse.value!!.gender,
+                                birthYear = tietMypageAge.text.toString().toInt(),
+                                height = tietMypageHeight.text.toString().toInt(),
+                                weight = tietMypageWeight.text.toString().toInt()
+                            )
+                        )
+                    ) {
+
+                    } else {
+                        Toast.makeText(requireContext(), "모든 정보를 올바르게 입력해주세요", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/project/veganlife/mypage/ui/viewmodel/MypageViewmodel.kt
+++ app/src/main/java/com/project/veganlife/mypage/ui/viewmodel/MypageViewmodel.kt
@@ -1,5 +1,6 @@
 package com.project.veganlife.mypage.ui.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,6 +8,7 @@ import androidx.lifecycle.viewModelScope
 import com.project.veganlife.data.model.ApiResult
 import com.project.veganlife.data.model.ProfileResponse
 import com.project.veganlife.domain.usecase.ProfileGetUsecase
+import com.project.veganlife.mypage.data.model.ProfileModifyRequest
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -14,27 +16,74 @@ import javax.inject.Inject
 class MypageViewmodel @Inject constructor(
     private val profileGetUsecase: ProfileGetUsecase,
 ): ViewModel(){
-    private val _resultUserInfo = MutableLiveData<ApiResult<ProfileResponse>>()
-    val resultUserInfo: LiveData<ApiResult<ProfileResponse>> get() = _resultUserInfo
+    // 유저 정보 api Response
+    private val _profileInfoResponse = MutableLiveData<ProfileResponse>()
+    val profileInfoResponse: LiveData<ProfileResponse> get() = _profileInfoResponse
 
-    private val _nickname = MutableLiveData<String>()
-    val nickname: LiveData<String> get() = _nickname
+    // 유저 정보 수정 api Request
+    private val _profileInfoRequest = MutableLiveData<ProfileResponse>()
+    val profileInfoRequest: LiveData<ProfileResponse> get() = _profileInfoRequest
 
-    private val _email = MutableLiveData<String>()
-    val email: LiveData<String> get() = _email
+    // 유저 정보 상태 체크
+    private val _profileInfoState = MutableLiveData<Boolean>()
+    val profileInfoState: LiveData<Boolean> get() = _profileInfoState
 
-    private val _photo = MutableLiveData<String>()
-    val photo: LiveData<String> get() = _photo
+    // 비건 타입 밸류
+    private val _vegetarianType = MutableLiveData<String>()
+    val vegetarianType: LiveData<String> get() = _vegetarianType
+
+
+    private val NICKNAME_PATTERN = "[가-힣]{2,10}".toRegex()
 
     fun getUserInfo() {
         viewModelScope.launch {
-            _resultUserInfo.value = profileGetUsecase.invoke()
+            val response = profileGetUsecase.invoke()
+            when(response) {
+                is ApiResult.Error -> {
+                    val response = response.description
+                    Log.d("get User Info Error", response)
+                }
+                is ApiResult.Exception -> {
+                    Log.d(
+                        "recommended Exception",
+                        response.e.message ?: "No message available"
+                    )
+                }
+                is ApiResult.Success -> {
+                    setUserInfo(response.data)
+                }
+            }
         }
     }
 
     fun setUserInfo(userInfo: ProfileResponse) {
-        _nickname.value = userInfo.nickname
-        _email.value = userInfo.email
-        _photo.value = userInfo.imageUrl
+        _profileInfoResponse.value = userInfo
+    }
+
+    // 닉네임 조건식
+    private fun isNicknameValid(nickname: String): Boolean {
+        return nickname.matches(NICKNAME_PATTERN)
+    }
+
+    // 키 조건식
+    private fun isHeightValid(value: Int?): Boolean {
+        return value != null && value > 0 && value <= 200
+    }
+
+    // 몸무게 조건식
+    private fun isWeightValid(value: Int?): Boolean {
+        return value != null && value > 0 && value <= 150
+    }
+
+    fun isUserInfoStateCheck(userInfo: ProfileModifyRequest): Boolean {
+        val isNicknameValid =  isNicknameValid(userInfo.nickname)
+        val isHeightValid = isHeightValid(userInfo.height)
+        val isWeightValid = isWeightValid(userInfo.weight)
+
+        return isNicknameValid && isHeightValid && isWeightValid
+    }
+
+    fun setVeganType(type: String) {
+        _vegetarianType.value = type
     }
 }
