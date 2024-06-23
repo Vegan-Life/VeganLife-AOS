c33a0f725af6bd1be039eb0efa67b4d5c842f58c
june0103 <rhwns0103@naver.com>
2024-06-23 23:56:29 +0900

    Feat: 라이프체크 식품 데이터 등록 UseCase 구현 #103

---
+++ app/src/main/java/com/project/veganlife/lifecheck/domain/usecase/LifeCheckRegisterMealDataUseCase.kt
@@ -0,0 +1,14 @@
+package com.project.veganlife.lifecheck.domain.usecase
+
+import com.project.veganlife.data.model.ApiResult
+import com.project.veganlife.lifecheck.data.model.LifeCheckMealDataRequest
+import com.project.veganlife.lifecheck.domain.repository.LifeCheckRegisterMealDataRepository
+import javax.inject.Inject
+
+class LifeCheckRegisterMealDataUseCase @Inject constructor(
+    private val repository: LifeCheckRegisterMealDataRepository
+) {
+    suspend operator fun invoke(data: LifeCheckMealDataRequest): ApiResult<LifeCheckMealDataRequest> {
+        return repository.registerMealData(data)
+    }
+}
