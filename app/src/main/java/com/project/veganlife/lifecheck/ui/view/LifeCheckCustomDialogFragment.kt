9cfd14c2a39dc017538b9ce9599e4a64029358cd
June <66195205+june0103@users.noreply.github.com>
2024-07-07 03:07:54 +0900

    Design: 라이프체크 메뉴 수정 및 삭제 UI 구현 #109 (#111)
    * Feat: LifeCheckMenuModifyFragment.kt 추가 #109
    
    * Design: string.xml 내용 추가 #109
    
    * Design: 라이프체크 메뉴 수정 UI #109
    
    * Feat: LifeCheckCustomDialogFragment.kt 추가 #109
    
    * Design: 라이프체크 dialog UI #109
    
    * Design: 라이프체크 toolbar style 적용 #109

---
+++ app/src/main/java/com/project/veganlife/lifecheck/ui/view/LifeCheckCustomDialogFragment.kt
@@ -0,0 +1,20 @@
+package com.project.veganlife.lifecheck.ui.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.project.veganlife.R
+
+
+class LifeCheckCustomDialogFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_life_check_custom_dialog, container, false)
+    }
+
+}
