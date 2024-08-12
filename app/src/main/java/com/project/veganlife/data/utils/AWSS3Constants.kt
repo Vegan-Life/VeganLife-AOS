c0c54dd2b171a47b60a3a35a0847629075bc5e6e
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-08-13 04:49:17 +0900

    Remove: Aws s3 비사용으로 인한 삭제

--- app/src/main/java/com/project/veganlife/data/utils/AWSS3Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.project.veganlife.data.utils
-
-const val COMMUNITY_FOLDER_PATH = "community/"
-const val LIFECHECK_FOLDER_PATH = "lifecheck/"
-const val PROFILE_FOLDER_PATH = "profile/"
-const val RECIPE_FOLDER_PATH = "recipe/"
-
-const val Region = "ap-northeast-2"
-const val BucketName = "veganlife"
