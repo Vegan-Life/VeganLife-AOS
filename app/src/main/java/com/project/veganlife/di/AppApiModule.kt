c7ff2803c740190bc509626dd4e18778420731b8
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-08-19 21:54:40 +0900

    Feat: 프로필 수정 api module 구현 #74

--- app/src/main/java/com/project/veganlife/di/AppApiModule.kt
+++ app/src/main/java/com/project/veganlife/di/AppApiModule.kt
@@ -6,13 +6,13 @@ import com.project.veganlife.data.remote.RecipeLikeApi
 import com.project.veganlife.data.remote.RecipeLikeCancelApi
 import com.project.veganlife.data.remote.RecommendedIntakeGetApi
 import com.project.veganlife.lifecheck.data.remote.LifeCheckMealDataApi
-import com.project.veganlife.lifecheck.data.remote.LifeCheckMealDataPostApi
 import com.project.veganlife.lifecheck.data.remote.LifeCheckMonthlyCalorieGetApi
 import com.project.veganlife.lifecheck.data.remote.LifeCheckWeeklyCalorieGetApi
 import com.project.veganlife.lifecheck.data.remote.LifeCheckYearlyCalorieGetApi
 import com.project.veganlife.mypage.data.remote.MypageGetMyPostedCommentApi
 import com.project.veganlife.mypage.data.remote.MypageGetMyPostedFeedApi
 import com.project.veganlife.mypage.data.remote.MypageGetScrapedRecipeApi
+import com.project.veganlife.data.remote.ProfileAdd_ModifyApi
 import com.project.veganlife.mypage.data.remote.MypageWithDrawalApi
 import com.project.veganlife.signup.data.remote.SignupApi
 import dagger.Module
@@ -20,6 +20,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -92,5 +93,11 @@ class AppApiModule {
     @Provides
     fun provideLifeCheckMealDataApi(retrofit: Retrofit): LifeCheckMealDataApi {
         return retrofit.create(LifeCheckMealDataApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideProfileAdd_ModifyApi(retrofit: Retrofit): ProfileAdd_ModifyApi {
+        return retrofit.create(ProfileAdd_ModifyApi::class.java)
     }
 }
