38995cc45c816245760ddc5263c5fc18568e4197
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-11-11 18:49:20 +0900

    Feat: recipeHomeFragment 기능 구현 #76

--- app/src/main/java/com/project/veganlife/recipe/ui/view/RecipeHomeFragment.kt
+++ app/src/main/java/com/project/veganlife/recipe/ui/view/RecipeHomeFragment.kt
@@ -1,19 +1,163 @@
 package com.project.veganlife.recipe.ui.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.project.veganlife.R
+import com.project.veganlife.databinding.FragmentRecipeHomeBinding
+import com.project.veganlife.recipe.data.model.RecipeFeedContent
+import com.project.veganlife.recipe.ui.adapter.RecipeHomeAdapter
+import com.project.veganlife.recipe.ui.viewmodel.RecipeHomeViewmodel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-class RecipeHomeFragment : Fragment() {
+@AndroidEntryPoint
+class RecipeHomeFragment : Fragment(), RecipeHomeAdapter.OnItemClickListener {
+    private var _binding: FragmentRecipeHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewmodel: RecipeHomeViewmodel by viewModels()
+    private lateinit var recipeHomeAdapter: RecipeHomeAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe_home, container, false)
+        _binding = FragmentRecipeHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 툴바 설정
+        setToolbarMove()
+
+        // 최상단일 시 스크롤버튼 숨김
+        hideGoToScroll()
+
+        // Scroll to top
+        binding.fbRecipeScrollUp.setOnClickListener { goToTopScroll() }
+
+        // 리싸이클러뷰 셋팅
+        setRecipeFeedssList()
+
+        viewmodel.getAllRecipeFeedsList()
+
+        // 비건 타입 버튼 필터
+        getRecipeList()
+
+        binding.btnRecipeWrite.setOnClickListener {
+            findNavController().navigate(R.id.action_recipeHomeFragment_to_recipeWriteFragment)
+        }
+    }
+
+    private fun setToolbarMove() {
+        binding.tbRecipeToolbar.setOnMenuItemClickListener { menu ->
+            when (menu.itemId) {
+                R.id.recipe_search -> {
+                    findNavController().navigate(R.id.action_recipeHomeFragment_to_recipeSearchFragment)
+                }
+
+                R.id.recipe_notification -> {
+                    findNavController().navigate(R.id.action_recipeHomeFragment_to_alarmFragment)
+                }
+            }
+            false
+        }
+    }
+
+    private fun goToTopScroll() {
+        binding.rvRecipeRecipeList.smoothScrollToPosition(0)
+    }
+
+    private fun hideGoToScroll() {
+        binding.apply {
+            rvRecipeRecipeList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                    fbRecipeScrollUp.visibility =
+                        if (layoutManager.findFirstVisibleItemPosition() == 0 || recipeHomeAdapter.itemCount == 0)
+                            View.GONE else View.VISIBLE
+                }
+            })
+        }
+    }
+
+    private fun getRecipeList() {
+        viewmodel.apply {
+            binding.rgRecipeVeganType.setOnCheckedChangeListener { _, i ->
+                when (i) {
+                    R.id.rb_recipe_home_all_type -> getAllRecipeFeedsList()
+
+                    R.id.rb_recipe_home_vegan -> getRecipeFeedByTypeList("VEGAN")
+
+                    R.id.rb_recipe_home_lacto -> getRecipeFeedByTypeList("LACTO")
+
+                    R.id.rb_recipe_home_ovo -> getRecipeFeedByTypeList("OVO")
+
+                    R.id.rb_recipe_home_lacto_ovo -> getRecipeFeedByTypeList("LACTO_OVO")
+
+                    R.id.rb_recipe_home_pesco -> getRecipeFeedByTypeList("PESCO")
+                }
+                goToTopScroll()
+            }
+        }
+    }
+
+    override fun onItemCLicked(item: RecipeFeedContent) {
+        val bundle = Bundle().apply {
+            putInt("recipeFeedId", item.id)
+        }
+        findNavController().navigate(
+            R.id.action_recipeHomeFragment_to_recipeDetailInfoFragment,
+            bundle
+        )
+    }
+
+    private fun setRecyclerviewAdapter() {
+        recipeHomeAdapter = RecipeHomeAdapter(this)
+        binding.rvRecipeRecipeList.adapter = recipeHomeAdapter
+
+        recipeHomeAdapter.addLoadStateListener { loadState ->
+//            val isEndOfPaginationReached = loadState.append.endOfPaginationReached && loadState.refresh is LoadState.NotLoading
+            val isEndOfPaginationReached = loadState.append.endOfPaginationReached
+            Log.d("itemCnt", recipeHomeAdapter.itemCount.toString())
+            if (isEndOfPaginationReached) {
+                binding.llRecipeNoContents.isVisible = recipeHomeAdapter.itemCount == 0
+            } else {
+                binding.llRecipeNoContents.isVisible = false
+            }
+        }
+    }
+
+    private fun setRecipeFeedssList() {
+        setRecyclerviewAdapter()
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewmodel.recipeFeedList.collectLatest { pagingData ->
+
+                pagingData?.let { recipePagingData ->
+                    recipeHomeAdapter.submitData(recipePagingData)
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
