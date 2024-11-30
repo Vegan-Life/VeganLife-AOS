f043205c4ce323f0caab20b03e5c77792177503d
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-12-01 03:35:31 +0900

    Feat: 비건 타입 칼라 Util 추가 #76

--- app/src/main/java/com/project/veganlife/utils/ui/VeganTypeChange.kt
+++ app/src/main/java/com/project/veganlife/utils/ui/VeganTypeChange.kt
@@ -1,5 +1,7 @@
 package com.project.veganlife.utils.ui
 
+import com.project.veganlife.R
+
 class VeganTypeChange {
     companion object {
         fun changeVeganType(type: String): String {
@@ -9,6 +11,16 @@ class VeganTypeChange {
                 "LACTO" -> "락토"
                 "PESCO" -> "페스코"
                 else -> "락토오보"
+            }
+        }
+
+        fun changeBackground(type: String): Int {
+            return when(type) {
+                "VEGAN" -> R.drawable.all_vegan_type_vegan_background
+                "LACTO" -> R.drawable.all_vegan_type_lacto_background
+                "OVO" -> R.drawable.all_vegan_type_ovo_background
+                "PESCO" -> R.drawable.all_vegan_type_pesco_background
+                else -> R.drawable.all_vegan_type_lacto_ovo_background
             }
         }
     }
