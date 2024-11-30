0296f4adf48d3fd0f9dabda38b209175425bec93
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-12-01 03:32:30 +0900

    Remove: 파일 삭제 #76

--- app/src/main/java/com/project/veganlife/data/datasource/RecipeLikeDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.project.veganlife.data.datasource
-
-import com.project.veganlife.data.model.ApiResult
-
-interface RecipeLikeDataSource {
-    suspend fun setRecipeLike(id: Long): ApiResult<Int>
-
-    suspend fun setRecipeLikeCancel(id: Long): ApiResult<Int>
-}
