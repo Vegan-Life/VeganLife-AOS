2c665009950fd2e8568c3ccc67f065e52a68ac95
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-12-18 17:32:21 +0900

    Feat: 레시피 상세보기 이미지 viewPager 구현

---
+++ app/src/main/java/com/project/veganlife/recipe/ui/adapter/RecipeFeedImagesViewPagerAdapter.kt
@@ -0,0 +1,62 @@
+package com.project.veganlife.recipe.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.project.veganlife.R
+import com.project.veganlife.databinding.ItemRecipeImageBinding
+
+class RecipeFeedImagesViewPagerAdapter :
+    ListAdapter<String, RecipeFeedImagesViewPagerAdapter.ViewPagerViewHolder>(diffUtil) {
+
+    inner class ViewPagerViewHolder(private val binding: ItemRecipeImageBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+            fun bind(url: String) {
+                // Glide로 이미지 로드
+                Glide.with(binding.imageView1.context)
+                    .load(url)
+                    .placeholder(R.drawable.all_spoon_fork_small) // 로드 전 기본 이미지
+                    .error(R.color.sub_gray2) // 로딩 실패 시 기본 색상
+                    .centerCrop()
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .into(binding.imageView1)
+
+            }
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
+        val view = ItemRecipeImageBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewPagerViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<String>() {
+            override fun areItemsTheSame(
+                oldItem: String,
+                newItem: String,
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: String,
+                newItem: String,
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
