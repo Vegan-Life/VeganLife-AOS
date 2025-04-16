8e0dee378e611453b4c7a587dc0cd391ac2c07c4
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2025-04-16 20:51:43 +0900

    Fix: 화면 이동 시 null 포인트 오류 수정

--- app/src/main/java/com/project/veganlife/mypage/ui/adapter/MypagePostedFeedAdapter.kt
+++ app/src/main/java/com/project/veganlife/mypage/ui/adapter/MypagePostedFeedAdapter.kt
@@ -55,7 +55,7 @@ class MypagePostedFeedAdapter :
 
                 clMypageLayout.setOnClickListener {
                     val bundle = Bundle()
-                    bundle.putLong("feedId", postedFeed.id)
+                    bundle.putInt("postId", postedFeed.id.toInt())
                     itemView.findNavController().navigate(
                         R.id.action_mypageWrotedFeedFragment_to_communityDetailFeedFragment,
                         bundle
