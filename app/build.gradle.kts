47fd3ad04bdeb3f237aa5316207c1d5e84538167
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2025-05-20 14:21:51 +0900

    Feat: 회원가입 닉네임 패턴 추가 (#183)
    * Feat: 회원가입 닉네임 숫자 패턴 추가
    
    * Chore: version code update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.project.veganlife"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.1"
+        versionCode = 5
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
