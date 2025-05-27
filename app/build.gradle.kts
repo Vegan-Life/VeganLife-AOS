96ef065c95678e80d8f0f72283b07f0b6186869e
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2025-05-27 21:27:55 +0900

    Chore: update versionCode (#188)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.project.veganlife"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.0.2"
+        versionCode = 6
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
